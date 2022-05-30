6d569d2419e0178a8b4125734f45576e8339c9d3
anton <anton-serozhechkin>
2022-05-31 01:38:25 +0300

    EU-9 delete useless constraint, add not null borders, make structure for step-by-step running

--- migrations/main.sql
+++ migrations/main.sql
@@ -1,11 +1,11 @@
 -- define postgres chema"
 SET search_path to public;
 
---Сreate table university"
--- university_pk marked as PK to university_id"
+--Сreate table university
+-- university_pk marked as PK to university_id
 CREATE TABLE university(
     university_id integer NOT NULL,
-    university_name varchar(255),
+    university_name varchar(255) NOT NULL,
     logo varchar(255),
     CONSTRAINT university_pk PRIMARY KEY (university_id));
 
@@ -17,14 +17,14 @@ ALTER TABLE university ALTER COLUMN university_id SET DEFAULT
 nextval('university_id_seq');
 
 -- Сreate table faculty
--- faculty_pk marked as PK to faculty_id"
+-- faculty_pk marked as PK to faculty_id
 CREATE TABLE faculty(
     faculty_id integer NOT NULL,
-    name varchar(255),
+    name varchar(255) NOT NULL,
     shortname varchar(20),
     main_email varchar(50),
-    hostel_email varchar(50),
-    university_id integer,
+    hostel_email varchar(50) NOT NULL,
+    university_id integer NOT NULL,
     CONSTRAINT faculty_pk PRIMARY KEY (faculty_id));
 
 -- Create sequence faculty_id_seq
@@ -36,20 +36,18 @@ ALTER TABLE faculty ALTER COLUMN faculty_id SET DEFAULT
 nextval('faculty_id_seq');
 
 -- Mark faculty_university_fk as a FK to university_id"
-ALTER TABLE faculty ADD CONSTRAINT faculty_university_fk FOREIGN KEY (university_id)
-REFERENCES university (university_id) 
-MATCH FULL
-ON DELETE CASCADE 
-ON UPDATE CASCADE;
+ALTER TABLE faculty ADD CONSTRAINT faculty_university_fk 
+FOREIGN KEY (university_id) REFERENCES university (university_id) 
+MATCH FULL ON DELETE CASCADE ON UPDATE CASCADE;
 
 
--- Create table student"
+-- Create table student
 -- student_pk marked as PK to faculty_id"
 CREATE TABLE student (
     student_id integer NOT NULL,
-    full_name varchar(255),
-    telephone_number varchar(255),
-    faculty_id integer,
+    full_name varchar(255) NOT NULL,
+    telephone_number varchar(255) NOT NULL,
+    faculty_id integer NOT NULL,
     user_id integer,
     CONSTRAINT student_pk PRIMARY KEY(student_id));
 
@@ -62,28 +60,20 @@ START WITH 1 INCREMENT BY 1;
 ALTER TABLE student ALTER COLUMN student_id SET DEFAULT
 nextval('student_id_seq');
 
--- Mark faculty_id as FK to faculty(faculty_id)"
-ALTER TABLE student ADD CONSTRAINT student_faculty_fk FOREIGN KEY (faculty_id)
-REFERENCES faculty(faculty_id)
-MATCH FULL
-ON DELETE CASCADE
-ON UPDATE CASCADE; 
+-- Mark student.faculty_id as FK to faculty(faculty_id)
+ALTER TABLE student ADD CONSTRAINT student_faculty_fk 
+FOREIGN KEY (faculty_id) REFERENCES faculty(faculty_id)
+MATCH FULL ON DELETE CASCADE ON UPDATE CASCADE; 
 
--- Mark student_user_fk as FK to user(user_id)"
-ALTER TABLE student ADD CONSTRAINT student_user_fk FOREIGN KEY (user_id)
-REFERENCES "user"(user_id)
-MATCH FULL
-ON DELETE CASCADE
-ON UPDATE CASCADE;
 
 -- Create table user
 CREATE TABLE "user"(
     user_id integer NOT NULL,
-    login varchar(50),
-    password varchar(50),
+    login varchar(50) NOT NULL,
+    password varchar(50) NOT NULL,
     last_visit timestamp,
-    email varchar(100),
-    role_id integer,
+    email varchar(100) NOT NULL,
+    role_id integer NOT NULL,
     is_active BOOLEAN DEFAULT FALSE,
     CONSTRAINT user_pk PRIMARY KEY(user_id));
 
@@ -95,17 +85,15 @@ START WITH 1 INCREMENT BY 1;
 ALTER TABLE "user" ALTER COLUMN user_id SET DEFAULT
 nextval('user_id_seq');
 
--- Mark user_faculty_fk as FK to faculty(faculty_id)"
-ALTER TABLE user ADD CONSTRAINT user_faculty_fk FOREIGN KEY(faculty_id)
-REFERENCES faculty(faculty_id)
-MATCH FULL
-ON DELETE CASCADE
-ON UPDATE CASCADE;
+-- Mark student_user_fk as FK to user(user_id)
+ALTER TABLE student ADD CONSTRAINT student_user_fk 
+FOREIGN KEY (user_id) REFERENCES "user"(user_id)
+MATCH FULL ON DELETE CASCADE ON UPDATE CASCADE;
 
--- Create table role"
+-- Create table role
 CREATE TABLE role(
-    role_id integer,
-    role_name varchar(50),
+    role_id integer NOT NULL,
+    role_name varchar(50) NOT NULL,
     CONSTRAINT role_pk PRIMARY KEY(role_id));
 
 -- Create sequence role_id_seq"
@@ -117,17 +105,15 @@ ALTER TABLE role ALTER COLUMN role_id SET DEFAULT
 nextval('role_id_seq');
 
 -- Mark user_role_fk as FK to role(role_id)"
-ALTER TABLE "user" ADD CONSTRAINT user_role_fk FOREIGN KEY(role_id)
-REFERENCES role(role_id)
-MATCH FULL
-ON DELETE CASCADE
-ON UPDATE CASCADE;
+ALTER TABLE "user" ADD CONSTRAINT user_role_fk 
+FOREIGN KEY(role_id) REFERENCES role(role_id)
+MATCH FULL ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- Create table actions"
 CREATE TABLE action(
-    role_id integer, 
-    action_name varchar(50),
-    action_id integer,
+    role_id integer NOT NULL, 
+    action_name varchar(50) NOT NULL,
+    action_id integer NOT NULL,
     CONSTRAINT action_pk PRIMARY KEY(action_id));
 
 -- Create sequence action_id_seq"
@@ -139,11 +125,9 @@ ALTER TABLE action ALTER COLUMN action_id SET DEFAULT
 nextval('action_id_seq');
 
 -- Mark action_role_fk as FK to action(role(id)"
-ALTER TABLE action ADD CONSTRAINT action_role_fk FOREIGN KEY(role_id)
-REFERENCES role(role_id)
-MATCH FULL
-ON DELETE CASCADE
-ON UPDATE CASCADE;
+ALTER TABLE action ADD CONSTRAINT action_role_fk 
+FOREIGN KEY(role_id) REFERENCES role(role_id)
+MATCH FULL ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- Create table user_faculty
 CREATE TABLE user_faculty(
@@ -152,15 +136,11 @@ CREATE TABLE user_faculty(
     CONSTRAINT user_faculty_pk PRIMARY KEY(user_id, faculty_id));
 
 --Mark user_faculty as FK to user(user_id)
-ALTER TABLE user_faculty ADD CONSTRAINT user_faculty_user_fk FOREIGN KEY(user_id)
-REFERENCES "user"(user_id)
-MATCH FULL
-ON DELETE CASCADE
-ON UPDATE CASCADE; 
+ALTER TABLE user_faculty ADD CONSTRAINT user_faculty_user_fk 
+FOREIGN KEY(user_id) REFERENCES "user"(user_id)
+MATCH FULL ON DELETE CASCADE ON UPDATE CASCADE; 
 
 -- Mark user_faculty as FK faculty(faculty_id)
-ALTER TABLE user_faculty ADD CONSTRAINT user_faculty_faculty_fk FOREIGN KEY(faculty_id)
-REFERENCES faculty(faculty_id)
-MATCH FULL
-ON DELETE CASCADE
-ON UPDATE CASCADE;
+ALTER TABLE user_faculty ADD CONSTRAINT user_faculty_faculty_fk 
+FOREIGN KEY(faculty_id) REFERENCES faculty(faculty_id)
+MATCH FULL ON DELETE CASCADE ON UPDATE CASCADE;
