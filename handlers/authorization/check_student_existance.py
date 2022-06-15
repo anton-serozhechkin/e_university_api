8468c10e219f0e7f10816e269a1b3b23e71ffdbf
Anton Serozhechkin <serezhechkintosh@gmail.com>
2022-06-15 14:38:41 +0300

    EU-6 add response model to check student endpoint, add field expires to response model

--- handlers/authorization/check_student_existance.py
+++ handlers/authorization/check_student_existance.py
@@ -1,11 +1,11 @@
 from models.student import student as student_table
 from models.one_time_token import one_time_token
 from db import database
+from settings.globals import TOKEN_LIFE_TIME
+
+from datetime import datetime, timedelta
 import hashlib
 import os
-from settings.globals import TOKEN_LIFE_TIME
-from datetime import datetime, timedelta
-
 
 from pydantic import BaseModel
 from fastapi import APIRouter
@@ -23,15 +23,18 @@ class StudentIn(BaseModel):
 class Student(BaseModel):
     student: int
     token: str
+    expires: datetime
 
 
+@router.post("/check-student-existance/", response_model=Student)
+async def check_student(student: StudentIn):
 
-@router.post("/check-student-existance/", response_model=None)
-async def create_faculty(student: StudentIn):
     query = student_table.select().where(student_table.c.full_name == student.full_name, student_table.c.telephone_number == student.telephone_number)
     result = await database.fetch_all(query)
+
     if not result:
-        return JSONResponse(status_code=404, content={"message": "Дані про студента не знайдено. Спробуйте ще раз."})
+        return JSONResponse(status_code=404, content={"message": "Дані про студента не знайдено. " \
+                                                                "Будь ласка, спробуйте ще раз."})
 
     for student in result: 
         student_id = student.student_id
@@ -42,9 +45,15 @@ async def create_faculty(student: StudentIn):
     query = one_time_token.insert().values(student_id=student_id, token=token,
                                            expires=expires).returning(one_time_token.c.token_id)                      
     last_record_id = await database.execute(query)
+
     query = one_time_token.select().where(one_time_token.c.token_id == last_record_id)
     result = await database.fetch_all(query)
-    for token in result:
-        response = {'token': token.token, 'student': token.student_id, 'expires': token.expires}
-    return response
 
+    for token in result:
+        response = {
+                    'token': token.token, 
+                    'student': token.student_id, 
+                    'expires': token.expires
+        }
+
+    return response
