bdcd0fa54f1ca5d65e87360ed8f83a9479a7a07e
dsherbakov <dmtr.shcherbakov@gmail.com>
2022-06-29 01:39:17 +0300

    EU-56_normal: apply all comments

---
+++ handlers/role.py
@@ -0,0 +1,16 @@
+from handlers.current_user import get_current_user
+from schemas.role import AvailableRoleOut
+from models.role import role as role_table 
+from db import database
+
+from typing import List
+
+from fastapi import Depends, APIRouter
+
+router = APIRouter()
+
+@router.get("/dispayrole/", response_model=List[AvailableRoleOut], tags=["Admin dashboard"])
+async def available_role(user = Depends(get_current_user)):
+    query = role_table.select()
+    return await database.fetch_all(query)
+    
---
+++ models/bed_places.py
@@ -0,0 +1,7 @@
+from sqlalchemy import (MetaData, Column, Table, Integer, VARCHAR)
+
+metadata_obj = MetaData()
+
+bed_places = Table('bed_places', metadata_obj,
+          Column('bed_place_id', Integer, primary_key=True),
+          Column('bed_place_name', VARCHAR(20)))
