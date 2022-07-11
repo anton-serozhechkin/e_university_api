6c7471e9e4c9a642923b5d09b49385d41394e243
dsherbakov <dmtr.shcherbakov@gmail.com>
2022-07-12 00:54:24 +0300

    EU-61: create endpoint to return user documents list

--- models/user_documents_view.py
+++ models/user_documents_view.py
@@ -5,6 +5,7 @@ metadata_obj = MetaData()
 
 
 user_documents_view = Table('user_documents_view', metadata_obj,
+          Column('user_document_id', Integer),
           Column('university_id', Integer),
           Column('user_id', Integer),
           Column('name', VARCHAR(255)),
--- schemas/user_documents.py
+++ schemas/user_documents.py
@@ -3,10 +3,9 @@ from datetime import datetime
 from pydantic import BaseModel
 
 class DocumentsListOut(BaseModel):
-    university_id: int
-    user_id: int
     user_document_id: int
     name: str
     date_created: datetime
     content: str
-    
+    user_id: int
+    university_id: int
