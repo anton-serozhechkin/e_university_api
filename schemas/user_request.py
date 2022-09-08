7b0f2d0fbc58868db1b4ea0e188bf12926d8d038
dsherbakov <dmtr.shcherbakov@gmail.com>
2022-09-09 01:05:31 +0300

    EU-68: hard work

--- schemas/user_request.py
+++ schemas/user_request.py
@@ -103,7 +103,3 @@ class HostelAccomodationViewOut(BaseModel):
     commandant_full_name: str
     telephone_number: str
     documents: Dict[str, str]
-
-
-class AccommodationOrderIn(BaseModel):
-    user_request_review_id: int
