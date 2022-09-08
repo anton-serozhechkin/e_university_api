9c918fb61007621dfc22fd6d352fc273984c77a4
dsherbakov <dmtr.shcherbakov@gmail.com>
2022-09-09 00:05:12 +0300

    EU-68: fix some bug design

--- handlers/speciality.py
+++ handlers/speciality.py
@@ -1,15 +1,16 @@
 from models.speciality_list_view import speciality_list_view
 from schemas.speciality import SpecialityListOut
-from db import database
 
+from db import database
 from typing import List
 
+
 from fastapi import APIRouter
+
 
 router = APIRouter()
 
 @router.get("/{university_id}/speciality/", response_model=List[SpecialityListOut], tags=["Admin dashboard"])
 async def read_speciality_list(university_id: int):
-
     query = speciality_list_view.select().where(speciality_list_view.c.university_id == university_id)                                  
     return await database.fetch_all(query)
