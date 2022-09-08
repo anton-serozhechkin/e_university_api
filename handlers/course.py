493f8543934937d3c0d6c850af91e904e637a2ee
dsherbakov <dmtr.shcherbakov@gmail.com>
2022-09-08 23:50:33 +0300

    EU-69: some code design fix

--- handlers/course.py
+++ handlers/course.py
@@ -1,14 +1,16 @@
 from models.course import course
 from schemas.course import CourseListOut
-from db import database
 
+from db import database
 from typing import List
 
 from fastapi import APIRouter
 
+
 router = APIRouter()
 
+
 @router.get("/courses/", response_model=List[CourseListOut], tags=["Admin dashboard"])
-async def courses_list():
+async def read_courses_list():
     query = course.select()
     return await database.fetch_all(query)
--- models/course_list_view.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import (MetaData, Column, Table, Integer)
-
-
-metadata_obj = MetaData()
-
-
-course_list_view = Table('course_list_view', metadata_obj,
-          Column('course_id', Integer),
-          Column('course_number', Integer))
