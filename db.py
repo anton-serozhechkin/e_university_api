9a2ae62480ddc08a8d8eb460ed61a4814b1f022c
egork <egorka.voitenko@gmail.com>
2022-10-07 18:02:53 +0300

    EU 83 Added settings for db (naming convention / declarative_base) (DRAFT)

--- db.py
+++ db.py
@@ -2,12 +2,23 @@ from settings import Settings
 
 import databases
 import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
 
+
+Base = declarative_base()
 
 database = databases.Database(Settings.POSTGRES_DSN)
 
-metadata = sqlalchemy.MetaData()
-
 engine = sqlalchemy.create_engine(Settings.POSTGRES_DSN)
+
+convention = {
+  "ix": "ix_%(column_0_label)s",
+  "uq": "uq_%(table_name)s_%(column_0_name)s",
+  "ck": "ck_%(table_name)s_%(constraint_name)s",
+  "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+  "pk": "pk_%(table_name)s"
+}
+
+metadata = sqlalchemy.MetaData(naming_convention=convention)
 
 metadata.create_all(engine)
