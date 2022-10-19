17818b333c5ebf487e3e8b427d3c54de7ca06beb
victro-nuzhniy <nuzhniyva@gmail.com>
2022-10-19 23:11:10 +0300

    EU-89 Changed error messages to english.

--- schemas/student.py
+++ schemas/student.py
@@ -26,47 +26,45 @@ class CreateStudentIn(BaseModel):
     @validator('full_name')
     def validate_full_name(value):  # TODO may be there is need to use staticmethod decorator
         full_name = value.split()   # TODO use hint or so
-        if not full_name:
-            raise BackendException(message="Прізвище та ім'я студента обов'язкові до заповнення!")
-        elif len(full_name) < 2:
-            raise BackendException(message="Прізвище та ім'я студента обов'язкові до заповнення!")
+        if not full_name or len(full_name) < 2:
+            raise BackendException(message="The student's name and surname are mandatory!")
         return value
     
     @validator('telephone_number')
     def validate_telephone_number(value):   # TODO may be there is need to use staticmethod decorator
         if not value:
-            raise BackendException(message='Телефонний номер не може бути порожнім!')
+            raise BackendException(message='The phone number cannot be empty!')
         elif len(str(value)) != 12:
-            raise BackendException(message='Телефонний номер має містити в собі 12 цифр!')
+            raise BackendException(message='The phone number must contain 12 digits!')
         return value
 
     @validator('course_id')
     def validate_course_id(value):  # TODO may be there is need to use staticmethod decorator
         if not value:
-            raise BackendException(message='Курс не може бути порожнім!')
+            raise BackendException(message='The course cannot be empty!')
         elif value not in range(1, 7):
-            raise BackendException(message='Курс моє бути між 1 та 6!')
+            raise BackendException(message='The course must be between 1 and 6!')
         return value
 
     @validator('speciality_id')
     def validate_speciality_id(value):
         if not value:
-            raise BackendException(message='Cпеціальність не може бути порожньою!')
+            raise BackendException(message='The specialty cannot be empty!')
         return value
 
     @validator('faculty_id')
     def validate_faculty_id(value):
         if not value:
-            raise BackendException(message='Факультет не може бути порожнім')
+            raise BackendException(message='The faculty cannot be empty!')
         return value
 
     @validator('gender')
     def validate_gender(value):
         exists_genders = ['Ч', 'М']
         if not value: 
-            raise BackendException(message='Стать студента не може бути порожня')
+            raise BackendException(message='The student gender cannot be empty!')
         if value.upper() not in exists_genders:     # TODO use hint or so
-            raise BackendException(message='Оберіть стать із запропонованого списку')
+            raise BackendException(message='Choose your gender from the list provided.')
         return value
 
 
