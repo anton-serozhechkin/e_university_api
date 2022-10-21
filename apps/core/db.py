cac1be908087ed514fa0ea8702f99c43931804c3
egork <egorka.voitenko@gmail.com>
2022-10-21 15:16:07 +0300

    [EU-78] Updated db file system(there were 2 db filed, united them + add new settings)

--- apps/core/db.py
+++ apps/core/db.py
@@ -1,7 +1,13 @@
-import re
+from settings import Settings
 
+import re
+import databases
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import MetaData
-from sqlalchemy.orm import declarative_base, declarative_mixin, declared_attr
+from sqlalchemy.orm import declarative_mixin, declared_attr
+
+database = databases.Database(Settings.POSTGRES_DSN)
 
 
 @declarative_mixin
@@ -14,12 +20,18 @@ class TableNameMixin:
     def __tablename__(cls) -> str:
         return cls.pattern.sub("_", cls.__name__).lower()
 
-NAMING_CONVENTION = {
+
+POSTGRES_INDEXES_NAMING_CONVENTION = {
     "ix": "ix_%(column_0_label)s",  # Index
     "uq": "uq_%(table_name)s_%(column_0_name)s",  # UniqueConstraint
     "ck": "ck_%(table_name)s_%(constraint_name)s",  # Check
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",  # ForeignKey
     "pk": "pk_%(table_name)s",  # PrimaryKey
 }
+Base = declarative_base(cls=TableNameMixin, metadata=MetaData(naming_convention=POSTGRES_INDEXES_NAMING_CONVENTION))
 
-Base = declarative_base(cls=TableNameMixin, metadata=MetaData(naming_convention=NAMING_CONVENTION))
+engine = sqlalchemy.create_engine(Settings.POSTGRES_DSN)
+
+metadata = sqlalchemy.MetaData(naming_convention=POSTGRES_INDEXES_NAMING_CONVENTION)
+
+metadata.create_all(engine)
