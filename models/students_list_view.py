af63f9981c535e93bc02796b826bbe14be7cd8aa
mnjl1 <d.manzhula@gmail.com>
2022-10-27 21:02:43 +0300

    EU-101 requested changes done

--- models/students_list_view.py
+++ models/students_list_view.py
@@ -1,4 +1,5 @@
-from sqlalchemy import (MetaData, Column, Table, Integer, VARCHAR) 
+from ast import Str
+from sqlalchemy import (MetaData, Column, Table, Integer, String, VARCHAR) 
 
 
 metadata_obj = MetaData()
@@ -7,7 +8,7 @@ metadata_obj = MetaData()
 students_list_view = Table('students_list_view', metadata_obj,
           Column('student_id', Integer),
           Column('student_full_name', VARCHAR(255)),
-          Column('telephone_number', Integer),
+          Column('telephone_number', String),
           Column('user_id', Integer),
           Column('university_id', Integer),
           Column('faculty_id', Integer),
