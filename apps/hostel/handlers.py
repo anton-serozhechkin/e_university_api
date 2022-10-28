8dde2a5be3928e7f632f5f3aa616788c2c07cc99
victro-nuzhniy <nuzhniyva@gmail.com>
2022-10-28 20:33:06 +0300

    EU-115 Added request, session dependencies to hostel router and handler

--- apps/hostel/handlers.py
+++ apps/hostel/handlers.py
@@ -1,14 +1,30 @@
 from apps.common.db import database
 from apps.hostel.models import BedPlace, hostel_list_view
+from apps.hostel.schemas import HostelListOut, BedPlaceOut
 
+from fastapi import Request
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
-async def read_hostels(university_id: int):
-    query = select(hostel_list_view).where(hostel_list_view.c.university_id == university_id)
-    return await database.fetch_all(query)
+class HostelHandler:
+
+    async def read_hostels_list(self,
+                                *,
+                                request: Request,
+                                university_id: int,
+                                session: AsyncSession,
+                                ) -> HostelListOut:
+        query = select(hostel_list_view).where(hostel_list_view.c.university_id == university_id)
+        return await database.fetch_all(query)
 
 
-async def read_available_bed_places():
-    query = select(BedPlace)
-    return await database.fetch_all(query)
+    async def read_available_bed_places(self,
+                                        *,
+                                        request: Request,
+                                        session: AsyncSession) -> BedPlaceOut:
+        query = select(BedPlace)
+        return await database.fetch_all(query)
+
+
+hostel_handler = HostelHandler()
--- apps/hostel/routers.py
+++ apps/hostel/routers.py
@@ -1,11 +1,13 @@
 from apps.hostel.schemas import HostelListOut, BedPlaceOut
 from apps.users.handlers import get_current_user
-from apps.hostel import handlers
+from apps.hostel.handlers import hostel_handler
+from apps.common.schemas import JSENDOutSchema, JSENDFailOutSchema
+from apps.common.dependencies import get_async_session
 
 from typing import List
 
-from fastapi import APIRouter, Depends
-from apps.common.schemas import JSENDOutSchema, JSENDFailOutSchema
+from fastapi import APIRouter, Depends, Request
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 hostel_router = APIRouter(
@@ -22,9 +24,14 @@ hostel_router = APIRouter(
                        422: {"model": JSENDFailOutSchema, "description": "ValidationError"}
                    },
                    tags=["Admin dashboard"])
-async def read_hostels(university_id: int, user=Depends(get_current_user)):
+async def read_hostels_list(request: Request,
+                            university_id: int,
+                            user=Depends(get_current_user),
+                            session: AsyncSession = Depends(get_async_session)):
     return {
-        "data": await handlers.read_hostels(university_id),
+        "data": await hostel_handler.read_hostels_list(request=request,
+                                                       university_id=university_id,
+                                                       session=session),
         "message": f"Got hostels list of the university with id {university_id}"
     }
 
@@ -35,8 +42,13 @@ async def read_hostels(university_id: int, user=Depends(get_current_user)):
                    summary="Get bed places list",
                    responses={200: {"description": "Successful get list of available bed places response"}},
                    tags=["Admin dashboard"])
-async def available_bed_places(user=Depends(get_current_user)):
+async def available_bed_places(request: Request,
+                               session: AsyncSession = Depends(get_async_session),
+                               user=Depends(get_current_user)):
     return {
-        "data": await handlers.read_available_bed_places(),
+        "data": await hostel_handler.read_available_bed_places(
+            request=request,
+            session=session
+        ),
         "message": "Got available bed places list"
     }
