8c64b9c554946c2c751021f12d9f5a5c44b5b3e6
Serozhechkin <serezhechkintosh@gmail.com>
2022-11-06 00:03:07 +0200

    EU-115 change \ to () for imports

--- apps/services/routers.py
+++ apps/services/routers.py
@@ -1,9 +1,12 @@
 from apps.common.dependencies import get_async_session, get_current_user
 from apps.common.schemas import JSENDFailOutSchema, JSENDOutSchema
 from apps.services.handlers import service_handler
-from apps.services.schemas import UserRequestExistenceOut, UserRequestsListOut, CreateUserRequestOut, \
-    CreateUserRequestIn, UserRequestBookingHostelOut, CancelRequestOut, CancelRequestIn, UserRequestReviewOut, \
-    UserRequestReviewIn, HostelAccomodationViewOut, UserRequestDetailsViewOut
+from apps.services.schemas import (UserRequestExistenceOut, UserRequestsListOut, 
+                                    CreateUserRequestOut, CreateUserRequestIn, 
+                                    UserRequestBookingHostelOut, CancelRequestOut, 
+                                    CancelRequestIn, UserRequestReviewOut,
+                                    UserRequestReviewIn, HostelAccomodationViewOut, 
+                                    UserRequestDetailsViewOut)
 
 from fastapi import APIRouter, Depends, Request
 from sqlalchemy.ext.asyncio import AsyncSession
