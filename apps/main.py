f7d5bda41af7cf60c564ad38a2f957666b325921
Serozhechkin <serezhechkintosh@gmail.com>
2022-12-20 00:41:27 +0200

    EU-79 formatted some separated files using black

--- apps/main.py
+++ apps/main.py
@@ -1,6 +1,9 @@
 from apps.authorization.routers import authorization_router
-from apps.common.exception_handlers import (backend_exception_handler, http_exception_handler,
-                                            validation_exception_handler)
+from apps.common.exception_handlers import (
+    backend_exception_handler,
+    http_exception_handler,
+    validation_exception_handler,
+)
 from apps.common.exceptions import BackendException
 from apps.educational_institutions.routers import educational_institutions_router
 from apps.hostel.routers import hostel_router
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,21 +5,21 @@ import uvicorn
 
 # Debugging
 reload = False
-reload_engine = 'auto'
+reload_engine = "auto"
 spew = False
 check_config = False
 print_config = False
 
 
 # Server socket
-HOST = f'{Settings.HOST}:{Settings.PORT}'
-POST = '5432'
+HOST = f"{Settings.HOST}:{Settings.PORT}"
+POST = "5432"
 backlog = 2048
 
 
 # Worker processes
 workers = 4
-worker_class = 'uvicorn.workers.UvicornWorker'
+worker_class = "uvicorn.workers.UvicornWorker"
 worker_connections = 1000
 max_requests = 100
 max_requests_jitter = 4
@@ -38,16 +38,16 @@ tmp_upload_dir = None
 
 
 # Logging
-accesslog = '-'
+accesslog = "-"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
-errorlog = '-'
-loglevel = 'info'
+errorlog = "-"
+loglevel = "info"
 capture_output = False
 
 
 # Process Naming
 proc_name = None
-default_proc_name = 'gunicorn'
+default_proc_name = "gunicorn"
 
 
 # Server Hooks
