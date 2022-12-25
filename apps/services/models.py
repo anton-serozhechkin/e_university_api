dffa89c91575e5db95d26ed0d95c01038d538734
Serozhechkin <serezhechkintosh@gmail.com>
2022-12-25 15:21:19 +0200

    EU-114 update readme, add some comments, fix issues with wrong date defining

--- apps/services/models.py
+++ apps/services/models.py
@@ -110,7 +110,7 @@ class UserRequestReview(Base):
 
     def __repr__(self):
         return f'{self.__class__.__name__}(user_request_review_id="{self.user_request_review_id}",date_created="{self.created_at}",' \
-               f'room_number="{self.room_number}", start_date_accommodation="{self.start_accommodation_date}",end_date_accommodation="{self.end_accommodation_date}", total_sum="{self.total_sum}", payment_deadline="{self.payment_deadline_date}",' \
+               f'room_number="{self.room_number}", start_date_accommodation="{self.start_accommodation_date}",end_date_accommodation="{self.end_accommodation_date}", total_sum="{self.total_sum}", payment_deadline_date="{self.payment_deadline_date}",' \
                f'remark="{self.remark}", bed_place_id="{self.bed_place_id}",reviewer="{self.reviewer}",hostel_id="{self.hostel_id}",university_id="{self.university_id}",user_request_id="{self.user_request_id}")'
 
 
