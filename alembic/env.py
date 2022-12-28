3accacc99510133e219cbbad7036b20b59dac22c
victro-nuzhniy <nuzhniyva@gmail.com>
2022-12-28 14:22:28 +0200

    EU-81 Changes in alembic.ini and env.py (checking functionality)

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,5 @@
+from settings import Settings
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -8,6 +10,9 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+section = config.config_ini_section
+config.set_section_option(section, "POSTGRES_DB_URL", Settings.POSTGRES_DSN)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
