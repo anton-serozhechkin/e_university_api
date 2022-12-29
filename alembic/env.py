d5b11002b627e7c43724725d888e01139ffd2d05
victro-nuzhniy <nuzhniyva@gmail.com>
2022-12-29 19:23:43 +0200

    EU-81 changed alembic.ini and env.py, added sqlalchemy-utils and python-dateutil to pyproject.toml

--- alembic/env.py
+++ alembic/env.py
@@ -1,11 +1,8 @@
 import pathlib
 
-from settings import Settings
-
 from logging.config import fileConfig
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
 from alembic import context
+from sqlalchemy_utils import database_exists, create_database
 
 from apps.authorization.models import Base as AuthBase
 from apps.educational_institutions.models import Base as EduBase
@@ -14,28 +11,15 @@ from apps.services.models import Base as ServiceBase
 from apps.users.models import Base as UserBase
 from apps.common.db import Base, engine
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-# section = config.config_ini_section
-# config.set_section_option(section, "POSTGRES_DB_URL", Settings.POSTGRES_DSN)
-
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+if not database_exists(engine.url):
+    create_database(engine.url)
 
 
 def run_migrations_offline() -> None:
@@ -55,7 +39,6 @@ def run_migrations_offline() -> None:
     if not SQL_VERSIONS_DIR.exists():
         SQL_VERSIONS_DIR.mkdir()
 
-    # url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=engine.url,
         target_metadata=target_metadata,
@@ -80,11 +63,6 @@ def run_migrations_online() -> None:
     connectable = context.config.attributes.get("connections", None)
     if connectable is None:
         connectable = engine
-    # connectable = engine_from_config(
-    #     config.get_section(config.config_ini_section),
-    #     prefix="sqlalchemy.",
-    #     poolclass=pool.NullPool,
-    # )
 
     with connectable.connect() as connection:
         context.configure(
