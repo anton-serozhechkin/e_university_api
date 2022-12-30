55ce4242604b5a8d428ac9ffb7dda19114154493
YehorVoitenko <egorka.voitenko@gmail.com>
2022-12-30 22:12:56 +0200

    [EU-128] Upgraded course validation to not access float values

--- apps/services/schemas.py
+++ apps/services/schemas.py
@@ -42,7 +42,7 @@ class CreateCustomHostelAccommodationIn(BaseInSchema):
         names = ['rector_first_name', 'rector_middle_name', 'rector_last_name', 'student_first_name',
                  'student_middle_name', 'student_last_name']
         for name in names:
-            if isinstance(values.get(name), str): # TODO: Upgrade validation system: can't know what value type is (thinks all is str)
+            if not isinstance(values.get(name), str): # TODO: Upgrade validation system: can't know what value type is (thinks all is str)
                 raise ValueError(f"{name.replace('_', ' ').capitalize()} have to be string type")
         return values
 
@@ -74,7 +74,7 @@ class CreateCustomHostelAccommodationIn(BaseInSchema):
 
     @validator('course')
     def validate_course(cls, value):
-        if value not in range(1, 7):
+        if value not in [1, 2, 3, 4, 5, 6]:
             raise ValueError(f"Wrong {value} number. This course doesn't exist")
         return value
 
