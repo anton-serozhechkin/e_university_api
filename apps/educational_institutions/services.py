e8563bf4e8ae466e0920c15e7a5da676c05eda36
Serozhechkin <serezhechkintosh@gmail.com>
2023-01-01 22:07:29 +0200

    EU-116 added possibility to create new dean or define existing while creating new faculty

--- apps/educational_institutions/services.py
+++ apps/educational_institutions/services.py
@@ -1,11 +1,13 @@
 from apps.educational_institutions.models import (
     Course,
+    Dean,
     Faculty,
     faculty_list_view,
     speciality_list_view,
 )
 from apps.hostel.services import AsyncCRUDBase
 
+dean_service = AsyncCRUDBase(model=Dean)
 faculty_list_service = AsyncCRUDBase(model=faculty_list_view)
 faculty_service = AsyncCRUDBase(model=Faculty)
 speciality_list_service = AsyncCRUDBase(model=speciality_list_view)
