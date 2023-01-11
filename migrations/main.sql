c5158ff79faf3c1fc542ece1cfc33c780404e081
victro-nuzhniy <nuzhniyva@gmail.com>
2023-01-11 14:59:30 +0200

    EU-66 added view to main.sql

--- migrations/main.sql
+++ migrations/main.sql
@@ -672,3 +672,43 @@ CREATE VIEW user_documents_list_view AS
         ur.university_id,
         ud.user_document_id,
         ur.user_id;
+
+DROP VIEW IF EXISTS user_request_hostel_warrant_view;
+CREATE VIEW user_request_hostel_warrant_view AS
+    SELECT
+    urr.user_request_review_id,
+    urr.room_number as room_number,
+    urr.created_at as created_at,
+    urr.service_id as service_id,
+    h.number as hostel_number,
+    h.street as hostel_street,
+    h.build as hostel_street,
+    bp.bed_place_name as bed_place_name,
+    u.university_name as university_name,
+    u.logo as university_logo,
+    u.city as university_city,
+    ur.status_id as status_id,
+    json_build_object('last_name', s.last_name, 'first_name', s.first_name, 'middle_name', s.middle_name)
+            as student_full_name,
+    s.gender as student_gender,
+    f.shortname as faculty_shortname,
+    json_build_object('last_name', d.last_name, 'first_name', d.first_name, 'middle_name', d.middle_name)
+        as dean_full_name,
+    FROM
+        user_request_review urr
+    LEFT JOIN hostel h ON
+        urr.hostel_id = h.hostel_id
+    LEFT JOIN bed_place bp ON
+        urr.bed_place_id = bp.bed_place_id
+    LEFT JOIN university u ON
+        urr.university_id = u.university_id
+    LEFT JOIN user_request ur ON
+        urr.user_request_id = ur.user_request_id
+    LEFT JOIN "user" user ON
+        ur.user_id = user.user_id
+    LEFT JOIN student s ON
+        user.student = s.user_id
+    LEFT JOIN faculty f ON
+        ur.faculty_id = f.faculty_id
+    LEFT JOIN dean d ON
+        f.dean_id = d.dean_id
