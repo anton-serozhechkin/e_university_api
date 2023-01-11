a60be3e28122cfd58e29f67389395a6740d378de
egork <egorka.voitenko@gmail.com>
2023-01-11 19:44:17 +0200

    [EU-128] Black refactoring

--- apps/services/handlers.py
+++ apps/services/handlers.py
@@ -67,9 +67,7 @@ from settings import (
 
 class ServiceHandler:
     @classmethod
-    async def validate_faculty_name(
-        cls, faculty_name: str, university_id: int
-    ) -> bool:
+    async def validate_faculty_name(cls, faculty_name: str, university_id: int) -> bool:
         faculties = await get_faculty_list(university_id)
         if not faculty_name in create_faculty_list(faculties):
             raise ValueError(f"Wrong '{faculty_name}' name. This faculty doesn't exist")
@@ -77,7 +75,7 @@ class ServiceHandler:
 
     @classmethod
     async def validate_speciality_code(
-        cls, speciality_code: int, university_id: int
+            cls, speciality_code: int, university_id: int
     ) -> bool:
         specialities = await get_specialties_list(university_id)
         if not speciality_code in create_speciality_dict(specialities).keys():
@@ -88,7 +86,7 @@ class ServiceHandler:
 
     @classmethod
     async def validate_speciality_name(
-        cls, speciality_name: str, university_id: int
+            cls, speciality_name: str, university_id: int
     ) -> bool:
         specialities = await get_specialties_list(university_id)
         if not speciality_name in create_speciality_dict(specialities).values():
@@ -99,12 +97,12 @@ class ServiceHandler:
 
     @classmethod
     async def validate_speciality_name_and_speciality_code_correspondence(
-        cls, speciality_name: str, speciality_code: int, university_id: int
+            cls, speciality_name: str, speciality_code: int, university_id: int
     ) -> bool:
         specialities = await get_specialties_list(university_id)
         if not (
-            (speciality_code, speciality_name)
-            in create_speciality_dict(specialities).items()
+                (speciality_code, speciality_name)
+                in create_speciality_dict(specialities).items()
         ):
             raise ValueError(
                 f"Speciality code '{speciality_code}' "
@@ -115,25 +113,21 @@ class ServiceHandler:
 
     @classmethod
     async def check_speciality_and_faculty_validation_result(
-        cls,
-        faculty_name: str,
-        speciality_name: str,
-        speciality_code: int,
-        university_id: int,
+            cls,
+            faculty_name: str,
+            speciality_name: str,
+            speciality_code: int,
+            university_id: int,
     ) -> bool:
         if (
-            await cls.validate_faculty_name(faculty_name, university_id)
-            and await cls.validate_speciality_name(
-                speciality_name, university_id
-            )
-            and await cls.validate_speciality_code(
-                speciality_code, university_id
-            )
-            and await cls.validate_speciality_name_and_speciality_code_correspondence(
-                speciality_name,
-                speciality_code,
-                university_id,
-            )
+                await cls.validate_faculty_name(faculty_name, university_id)
+                and await cls.validate_speciality_name(speciality_name, university_id)
+                and await cls.validate_speciality_code(speciality_code, university_id)
+                and await cls.validate_speciality_name_and_speciality_code_correspondence(
+            speciality_name,
+            speciality_code,
+            university_id,
+        )
         ):
             return True
         else:
@@ -141,20 +135,20 @@ class ServiceHandler:
 
     @classmethod
     async def create_request_for_hostel_accommodation(
-        cls,
-        *,
-        request: Request,
-        university_id: int,
-        user_request: RequestForHostelAccommodationIn,
-        user: UserOut,
-        session: AsyncSession,
+            cls,
+            *,
+            request: Request,
+            university_id: int,
+            user_request: RequestForHostelAccommodationIn,
+            user: UserOut,
+            session: AsyncSession,
     ) -> RequestForHostelAccommodationOut:
 
         if await cls.check_speciality_and_faculty_validation_result(
-            user_request.faculty_name,
-            user_request.speciality_name,
-            user_request.speciality_code,
-            university_id,
+                user_request.faculty_name,
+                user_request.speciality_name,
+                user_request.speciality_code,
+                university_id,
         ):
             user_faculty = await user_faculty_service.read(
                 data={"user_id": user.user_id}, session=session
@@ -189,11 +183,11 @@ class ServiceHandler:
 
     @staticmethod
     async def read_user_documents_list(
-        *,
-        request: Request,
-        university_id: int,
-        user: UserOut,
-        session: AsyncSession,
+            *,
+            request: Request,
+            university_id: int,
+            user: UserOut,
+            session: AsyncSession,
     ) -> Optional[List[UserDocumenstListOut]]:
         return await user_documents_list_service.list(
             session=session,
@@ -202,12 +196,12 @@ class ServiceHandler:
 
     @staticmethod
     async def read_user_request_existence(
-        *,
-        request: Request,
-        university_id: int,
-        service_id: int,
-        user: UserOut,
-        session: AsyncSession,
+            *,
+            request: Request,
+            university_id: int,
+            service_id: int,
+            user: UserOut,
+            session: AsyncSession,
     ) -> UserRequestExistenceOut:
         user_request_result = await request_existence_service.read(
             session=session,
@@ -231,11 +225,11 @@ class ServiceHandler:
 
     @staticmethod
     async def read_user_request_list(
-        *,
-        request: Request,
-        university_id: int,
-        user: UserOut,
-        session: AsyncSession,
+            *,
+            request: Request,
+            university_id: int,
+            user: UserOut,
+            session: AsyncSession,
     ) -> List[UserRequestsListOut]:
         return await user_request_list_service.list(
             session=session,
@@ -244,11 +238,11 @@ class ServiceHandler:
 
     @staticmethod
     async def read_user_request_booking_hostel(
-        *,
-        request: Request,
-        university_id: int,
-        user: UserOut,
-        session: AsyncSession,
+            *,
+            request: Request,
+            university_id: int,
+            user: UserOut,
+            session: AsyncSession,
     ) -> UserRequestBookingHostelOut:
         return await user_request_booking_hostel_service.read(
             session=session,
@@ -257,11 +251,11 @@ class ServiceHandler:
 
     @staticmethod
     async def cancel_request(
-        *,
-        request: Request,
-        user_request_id: int,
-        cancel_request: CancelRequestIn,
-        session: AsyncSession,
+            *,
+            request: Request,
+            user_request_id: int,
+            cancel_request: CancelRequestIn,
+            session: AsyncSession,
     ) -> CreateUserRequestOut:
         CancelRequestIn(status_id=cancel_request.status_id)
         user_request = await user_request_service.update(
@@ -273,13 +267,13 @@ class ServiceHandler:
 
     @staticmethod
     async def create_user_request_review(
-        *,
-        request: Request,
-        university_id: int,
-        user_request_id: int,
-        user_request_review: UserRequestReviewIn,
-        user: UserOut,
-        session: AsyncSession,
+            *,
+            request: Request,
+            university_id: int,
+            user_request_id: int,
+            user_request_review: UserRequestReviewIn,
+            user: UserOut,
+            session: AsyncSession,
     ) -> UserRequestReviewOut:
         created_user_request_review = await user_request_review_service.create(
             session=session,
@@ -307,11 +301,11 @@ class ServiceHandler:
 
     @staticmethod
     async def read_hostel_accommodation(
-        *,
-        request: Request,
-        university_id: int,
-        user_request_id: int,
-        session: AsyncSession,
+            *,
+            request: Request,
+            university_id: int,
+            user_request_id: int,
+            session: AsyncSession,
     ) -> Optional[HostelAccomodationViewOut]:
         response = await hostel_accommodation_service.read(
             session=session,
@@ -323,11 +317,11 @@ class ServiceHandler:
 
     @staticmethod
     async def read_request_details(
-        *,
-        request: Request,
-        university_id: int,
-        user_request_id: int,
-        session: AsyncSession,
+            *,
+            request: Request,
+            university_id: int,
+            user_request_id: int,
+            session: AsyncSession,
     ) -> UserRequestDetailsViewOut:
         return await user_request_detail_service.read(
             session=session,
@@ -336,12 +330,12 @@ class ServiceHandler:
 
     @staticmethod
     async def read_user_document(
-        *,
-        request: Request,
-        university_id: int,
-        user_document_id: int,
-        user: UserOut,
-        session: AsyncSession,
+            *,
+            request: Request,
+            university_id: int,
+            user_document_id: int,
+            user: UserOut,
+            session: AsyncSession,
     ) -> str:
         user_document = await user_document_service.read(
             session=session, data={"user_document_id": user_document_id}
@@ -349,13 +343,13 @@ class ServiceHandler:
         return file_manager.get(user_document.content)
 
     async def download_user_document(
-        self,
-        *,
-        request: Request,
-        university_id: int,
-        user_document_id: int,
-        user: UserOut,
-        session: AsyncSession,
+            self,
+            *,
+            request: Request,
+            university_id: int,
+            user_document_id: int,
+            user: UserOut,
+            session: AsyncSession,
     ) -> Tuple[str, str]:
         user_document = await user_document_service.read(
             session=session, data={"user_document_id": user_document_id}
@@ -368,12 +362,12 @@ class ServiceHandler:
         return user_document.content, file_name
 
     async def count_hostel_accommodation_cost(
-        self,
-        *,
-        request: Request,
-        university_id: int,
-        data: CountHostelAccommodationCostIn,
-        session: AsyncSession,
+            self,
+            *,
+            request: Request,
+            university_id: int,
+            data: CountHostelAccommodationCostIn,
+            session: AsyncSession,
     ) -> CountHostelAccommodationCostOut:
         hostel = await hostel_service.read(
             session=session, data={"hostel_id": data.hostel_id}
@@ -397,27 +391,27 @@ class ServiceHandler:
 
     @classmethod
     def calculate_difference_between_dates_in_months(
-        cls, end_date: date, start_date: date
+            cls, end_date: date, start_date: date
     ) -> int:
         return (
-            end_date.month - start_date.month + 12 * (end_date.year - start_date.year)
-        ) + 1
+                       end_date.month - start_date.month + 12 * (end_date.year - start_date.year)
+               ) + 1
 
     @classmethod
     def get_month_price_by_bed_place(
-        cls, hostel_month_price: Decimal, bed_place_name: str
+            cls, hostel_month_price: Decimal, bed_place_name: str
     ) -> Decimal:
         return Decimal(hostel_month_price) * Decimal(bed_place_name)
 
     @classmethod
     def calculate_total_hostel_accommodation_cost(
-        cls, month_price: Decimal, month_difference: int
+            cls, month_price: Decimal, month_difference: int
     ) -> Decimal:
         return month_price * month_difference
 
     @classmethod
     async def __create_user_document(
-        cls, session: AsyncSession, **kwargs
+            cls, session: AsyncSession, **kwargs
     ) -> UserDocument:
         service_id = kwargs.get("service_id")
         name = await cls.__generate_user_document_name(service_id, session)
@@ -441,7 +435,7 @@ class ServiceHandler:
 
     @classmethod
     async def __create_user_document_content_hostel_settlement_service(
-        cls, **kwargs
+            cls, **kwargs
     ) -> str:
         context = kwargs.get("context")
         rendered_template = file_manager.render(
@@ -463,7 +457,7 @@ class ServiceHandler:
 
     @classmethod
     async def __generate_user_document_name(
-        cls, service_id: int, session: AsyncSession
+            cls, service_id: int, session: AsyncSession
     ) -> str:
         service = await service_service.read(
             session=session, data={"service_id": service_id}
@@ -472,7 +466,7 @@ class ServiceHandler:
 
     @classmethod
     async def __generate_user_document_name_for_download(
-        cls, document_name: str, user_id: int, session: AsyncSession
+            cls, document_name: str, user_id: int, session: AsyncSession
     ) -> str:
         student = await student_service.read(session=session, data={"user_id": user_id})
         return (
@@ -482,11 +476,11 @@ class ServiceHandler:
 
     @staticmethod
     async def create_students_list_from_file(
-        *,
-        request: Request,
-        university_id: int,
-        file: UploadFile = File(...),
-        session: AsyncSession,
+            *,
+            request: Request,
+            university_id: int,
+            file: UploadFile = File(...),
+            session: AsyncSession,
     ) -> Union[List[CreateStudentsListOut], None]:
         specialties, students = await get_specialties_list(university_id), []
 
